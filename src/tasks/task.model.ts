99f7cf68d5017cd123d8033f9d148d5e6ac4c222
NikhilBudaniya <92074627+NikhilBudaniya@users.noreply.github.com>
2022-10-20 01:51:12 +0530

    Added swagger

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -1,8 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
 export class Task{
+    public id:number;
+    public date_created:string;
+    @ApiProperty({description:"Title of the task."})
+    @IsNotEmpty()
+    public title: string;
+    public is_completed?:boolean;
     constructor(
-    public id:number,
-    public date_created:string,
-    public title: string,
-    public is_completed?:boolean,
-    ){}
+    id:number,
+    date_created:string,
+    title: string,
+    is_completed?:boolean,
+    ){
+        this.id=id,
+        this.date_created=date_created,
+        this.title=title,
+        this.is_completed=is_completed
+    }
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ApiAcceptedResponse, ApiBadGatewayResponse, ApiBadRequestResponse, ApiCreatedResponse } from '@nestjs/swagger';
 import { TaskService } from './task.service';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly taskService:TaskService){}
 
+    @ApiCreatedResponse({description:'Created a task successfully.'})
     @Post()
     addTask(@Body('title') taskTitle:string){
         const generatedTaskId=this.taskService.addTask(taskTitle);
@@ -15,12 +17,14 @@ export class TasksController {
     getAllTasks(){
         return this.taskService.getTasks();
     }
-
+    
+    
     @Get(':id')
     getTask(@Param('id') taskId:string){
         return this.taskService.getSingleTask(taskId);
     }
-
+    
+    @ApiCreatedResponse({description:'Successfully Updated the task.'})
     @Patch(':id')
     updateTask(@Param('id') taskId:string,@Body('title') taskTitle:string,@Body('is_completed') status:boolean){
         this.taskService.updateTask(taskId,taskTitle,status)
